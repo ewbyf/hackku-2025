c88aff1d9db98076e6aa613249e91d1b6d6f3b22
Jason Xu <Xinwen31415@gmail.com>
2025-04-05 00:53:52 -0500

    initial authentication/authorization api + db stuff

---
+++ backend/src/db/db.service.ts
@@ -0,0 +1,9 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class DBService extends PrismaClient implements OnModuleInit {
+	public async onModuleInit(): Promise<void> {
+		await this.$connect();
+	}
+}
---
+++ backend/src/users/users.dtos.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsString } from 'class-validator';
+import { fi } from 'src/utils/utils';
+
+export class LoginDTO {
+	@IsEmail()
+	email: string = fi();
+
+	@IsString()
+	password: string = fi();
+}
+
+export class RegisterDTO {
+	@IsEmail()
+	email: string = fi();
+
+	@IsString()
+	password: string = fi();
+}
