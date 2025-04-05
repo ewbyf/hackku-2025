3e173dfe26e40039250cb44bbe9798894e02cf59
Jason Xu <Xinwen31415@gmail.com>
2025-04-05 14:19:37 -0500

    added patient records accordion

--- frontend/app/home/documents.tsx
+++ frontend/app/home/documents.tsx
@@ -1,22 +1,65 @@
-import { useRouter } from 'expo-router';
+import AccordionItem from '@/components/AccordionItem';
+import Title from '@/components/Title';
+import TopBar from '@/components/TopBar';
+import { global } from '@/lib/context';
+import { Redirect, useRouter } from 'expo-router';
+import { useContext, useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import { StyleSheet, TouchableOpacity, View, Text, TextInput, SafeAreaView, Button } from 'react-native';
 
 export default function Documents() {
-    const router = useRouter();
+	const router = useRouter();
+	const { user } = useContext(global);
+	const [selected, setSelected] = useState<number | null>(null);
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <KeyboardAwareScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ flex: 1, padding: 20 }}>
-                <Text>Login</Text>
-            </KeyboardAwareScrollView>
-        </SafeAreaView>
-    );
+	if (!user) return <Redirect href="/login" />;
+
+	return (
+		<SafeAreaView style={styles.container}>
+			<TopBar />
+			<KeyboardAwareScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ padding: 20 }}>
+				<Title>Medical Records</Title>
+				<View style={styles.list}>
+					{user.procedures.map((proc, i) => (
+						<AccordionItem
+							short={
+								<Text style={styles.summary} numberOfLines={1} minimumFontScale={0.66} adjustsFontSizeToFit allowFontScaling>
+									{proc.explanation.technical}
+								</Text>
+							}
+							opened={selected === i}
+							onOpened={() => setSelected(i)}
+							onClosed={() => setSelected(null)}
+							key={i}>
+							<Text style={styles.details}>{proc.explanation.explanation}</Text>
+						</AccordionItem>
+					))}
+				</View>
+			</KeyboardAwareScrollView>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-    container: {
-        display: 'flex',
-        flex: 1,
-    },
+	container: {
+		display: 'flex',
+		flex: 1
+	},
+	summary: {
+		color: 'white',
+		fontSize: 30,
+		textOverflow: 'ellipsis',
+		overflow: 'hidden',
+		maxWidth: 250
+	},
+	list: {
+		display: 'flex',
+		flexDirection: 'column',
+		gap: 10
+	},
+	details: {
+		fontSize: 18,
+		lineHeight: 24
+	}
 });
+
---
+++ frontend/components/AccordionItem.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+interface Props {
+	short: React.ReactNode;
+	opened: boolean;
+	onOpened: () => void;
+	onClosed: () => void;
+}
+
+const AccordionItem: React.FC<React.PropsWithChildren<Props>> = ({ short, opened, onOpened, onClosed, children }) => {
+	return (
+		<View style={styles.container}>
+			<TouchableOpacity onPress={opened ? onClosed : onOpened}>
+				<View style={styles.box}>
+					<View>{short}</View>
+					<Icon name={opened ? 'chevron-up' : 'chevron-down'} color="white" size={30} />
+				</View>
+			</TouchableOpacity>
+			{opened && <View style={styles.details}>{children}</View>}
+		</View>
+	);
+};
+
+export default AccordionItem;
+
+const styles = StyleSheet.create({
+	container: {
+		display: 'flex',
+		flexDirection: 'column',
+		gap: 0,
+		color: 'white',
+		overflow: 'hidden',
+		borderBottomLeftRadius: 10,
+		borderBottomRightRadius: 10,
+		shadowOffset: { width: 0, height: 4 },
+		shadowOpacity: 0.25,
+		backgroundColor: 'white'
+	},
+	box: {
+		display: 'flex',
+		alignContent: 'center',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		paddingLeft: 20,
+		paddingRight: 20,
+		paddingTop: 15,
+		paddingBottom: 15,
+		backgroundColor: '#6C63FF',
+		borderTopLeftRadius: 10,
+		borderTopRightRadius: 10
+	},
+	icon: {
+		height: 30
+	},
+	details: {
+		paddingLeft: 20,
+		paddingRight: 20,
+		paddingTop: 10,
+		paddingBottom: 10
+	}
+});
+
